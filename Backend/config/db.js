472c6ff7967fda762607e521c89471c57bb391a6
dhanush <tdhanush8124@gmail.com>
2025-06-06 17:59:20 +0530

    commit-2

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,9 +1,16 @@
 import mongoose from "mongoose";
+import user from "../models/userModel.js"
 
 const connectDb =  async()=>{
   try{
     const conn = await mongoose.connect(process.env.MONGO_URI)
     console.log(`mongodb connected: ${conn.connection.host}`);
+    await user.create({
+      name : "abc",
+      email : "sfsf",
+      role : "admin",
+      password : "abcd"
+    });
   }
   catch(error){
     console.log(error.message);
