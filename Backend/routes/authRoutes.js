e790b517dae83b86a97f48b4443065a020d8fd5e
Viswa-S-2105 <sviswa2105@gmail.com>
2025-06-17 10:36:26 +0530

    Added Login and Register features

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,8 +1,10 @@
-import express from 'express'
-import { login } from '../controllers/auth/authControllers.js';
+import express from 'express';
+import registerUser from '../controllers/auth/registerControllers.js';
+import loginUser from '../controllers/auth/loginControllers.js';
 
-const authRoutes = express.Router();
+const router = express.Router();
 
-authRoutes.use('/login/who', login);
+router.post('/login', loginUser);
+router.post('/register', registerUser);
 
-export default authRoutes;
+export default router;
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Home from './pages/Home/Home';
 import ParticipantDashboard from './pages/Participants/dashboard';
 import CoordinatorDashboard from './pages/coordinator/dashboard';
+import RegisterForm from './pages/Auth/RegisterForm';
+import LoginForm from './pages/Auth/LoginForm';
 import HodDashboard from './pages/HOD/dashboard';
 import Feedback from './pages/Feedback/feedback';
 
@@ -11,6 +13,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
+        <Route path="/register/:role" element={<RegisterForm />} />
+        <Route path="/login/:role" element={<LoginForm />} />
         <Route path="/participant/dashboard" element={<ParticipantDashboard/>}></Route>
         <Route path="/coordinator/dashboard" element={<CoordinatorDashboard/>}></Route>
         <Route path="/hod/dashboard" element={<HodDashboard/>}></Route>
